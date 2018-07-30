12e741116d3738976cbf035f49520fc224340807
Rob <robert.mastronicola.ctr@us.af.mil>
2018-07-30 11:20:19 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -17,11 +17,23 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Rob',
+		text: 'Hey. What is going on', 
+		createdAt: new Date()
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('Client disconnected');
-
 	});
 });
+
+
+
 
 server.listen(port, () => {
 	console.log('Started on port ' + port);
