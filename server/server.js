c4d4168d39aa64d49b28624a82fff22e74296004
unknown <arejaycola@gmail.com>
2018-08-01 15:16:34 -0400

    Add Users class and update updateUserList event

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const socketIO = require('socket.io');
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
-
+const {Users} = require('./utils/users');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -20,7 +20,7 @@ var app = express();
 /*For socket.io configuration*/
 var server = http.createServer(app);
 var io = socketIO(server);
-
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -30,10 +30,14 @@ io.on('connection', (socket) => {
 	socket.on('join', (params, callback) => {
 		/* Validate the data */
 		if(!isRealString(params.name) || !isRealString(params.room)){
-			callback('Name and room name are requried');
+			return callback('Name and room name are requried');
 		}
 
 		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id, params.name, params.room);
+
+		io.to(params.room).emit('updateUserList', users.getUserList(params.room));
 
 		socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 		socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined the room`));
@@ -55,7 +59,12 @@ io.on('connection', (socket) => {
 	});	
 
 	socket.on('disconnect', () => {
-		console.log('Client disconnected');
+		var user = users.removeUser(socket.id);
+
+		if(user) {
+			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
+		}
 	});
 });
 
